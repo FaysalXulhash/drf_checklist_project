abad57d5646db9351a4887a00558c158d6bf05cb
FaysalXulhash <faysalahammed80@gmail.com>
2024-11-20 19:41:24 +0600

    first commit

---
+++ core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import CheckList, CheckListItem
+# Register your models here.
+
+
+@admin.register(CheckList)
+class CheckListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'is_deleted', 'is_archived', 'created_at', 'updated_at']
+
+
+@admin.register(CheckList)
+class CheckListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'checklist', 'is_checked', 'created_at', 'updated_at']
